9a8569f10550b16c2e36939f6b45b3079d9ceaca
srima <as9731@srmist.edu.in>
2024-06-12 23:22:50 +0530

    added contact section

--- script.js
+++ script.js
@@ -29,6 +29,14 @@ function type() {
     }
 }
 
+
+//downloading resume
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
     setTimeout(type, newTextDelay);
 
@@ -144,6 +152,20 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
 
     
 });
+
+
+//contact section
+
+function SendMail(){
+    var params={
+        from_name:document.getElementById("name").value,
+        email_id:document.getElementById("email").value,
+        message:document.getElementById("message").value
+    }
+    emailjs.send("service_y4180d3","template_9hnabm7",params).then(function(res){
+        alert("Success! ",res.status);
+    })
+}
 
 
 
